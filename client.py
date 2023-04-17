15f1531ad0ec12e16e8d49aabfe27c68fe40e957
aliabbasi_77 <aliabbasi7898@gmail.com>
2023-04-17 16:10:14 -0600

    changing local epoches

--- client.py
+++ client.py
@@ -103,11 +103,11 @@ class Client(object):
                 return 1
         elif(self.green == -1):
             if (self.carbonIntensity == 1000):
-                return 0.2
+                return 0.4
             elif (self.carbonIntensity == 10):
                 return 0.6
             else:
-                return 1
+                return 0.8
         elif (self.green == -3):
             if (self.carbonIntensity == 1000):
                 return 0.2
